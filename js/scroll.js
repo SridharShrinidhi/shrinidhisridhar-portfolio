6825c14b7aa8718959689d9330ee8eb8714bc3e3
Shrinidhi Sridhar <srid0002@algonquinlive.com>
2018-12-05 22:15:59 -0500

    Correct grammar and scroll
    Grammatical errors was corrected. Scroll was fixed

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,12 @@
 $(function() {
 
+  var hidden = false
+
    $("#main").mousewheel(function(event) {
+     if (!hidden) {
+       hidden = true
+       $(".arrow").hide()
+     }
      if (Math.abs(event.deltaY) > Math.abs(event.deltaX)) {
       this.scrollLeft -= event.deltaY * event.deltaFactor;
       event.preventDefault();
